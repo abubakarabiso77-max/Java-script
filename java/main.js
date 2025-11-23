27436b4d4365519e786b81563c6078cc8d95bd4f
abubakarabiso77-max <abubakarabiso77@gmail.com>
2025-11-23 16:12:55 +0100

    main.js

---
+++ java/main.js
@@ -0,0 +1,7 @@
+var a ="abiso"
+var b = a.toUpperCase()
+b=a.toLowerCase()
+console.log(b)
+var randomnum = Math.random() 
+var c=1.29
+console.log(Math.floor(c))
---
+++ main.js
@@ -0,0 +1,7 @@
+ function student(){
+      console.log("hello world");
+ }
+
+ var student2=function(){
+      console.log(abiso);
+ }
