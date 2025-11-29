c06776ff5ebc6043452ef6aa3076f0d2af4e0435
abubakarabiso77-max <abubakarabiso77@gmail.com>
2025-11-29 16:08:02 +0100

    script.js

---
+++ java script/script.js
@@ -0,0 +1,16 @@
+var studentDetail={
+    name1:"Abiso",
+    grade:9,
+    age:14
+}
+console.log(studentDetail["name1"])
+console.log(studentDetail.name1)
+
+console.log(Math.floor( 7.7))
+
+    let user = {
+        greet: function(name) {
+            return "hello"+name
+        }
+    }
+    console.log(user.greet("Abiso"))
