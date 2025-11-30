2d27b14cd402a5e2144bde8d85a9e20cf67e3ba2
abubakarabiso77-max <abubakarabiso77@gmail.com>
2025-11-30 16:23:28 +0100

    java script

---
+++ java script/Array and call/main.js
@@ -0,0 +1,22 @@
+var student = {
+    name: "lucksan",
+    age: 20,
+    hobby:
+
+}
+
+console.log (student["name"])
+console.log(studentname)
+
+var a = 2.576843
+console.log(math.round(a))
+var b = 2.345
+console.log(math.floor(b))
+
+var .fruits ={"banana,avocado,papaya,mango"}
+console.log(fruits[0])
+
+var joinarry = fruits.join ("*")
+console.log(joinarry)
+fruits.pop()
+console.log(furits)
