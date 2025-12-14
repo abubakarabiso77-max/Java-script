4324a2f6992f056ad57eff5bf5758412e3997952
abubakarabiso77-max <abubakarabiso77@gmail.com>
2025-12-14 16:08:46 +0100

    main.js

---
+++ classes in javascript/main.js
@@ -0,0 +1,13 @@
+
+const student ={
+    name:abubakar,
+    grade:9,
+    showinfo: function () {
+
+        console.log("name:" + this.name)
+        console.log("name:" + this.grade)
+
+    }
+}
+student.showinfo();
+//
